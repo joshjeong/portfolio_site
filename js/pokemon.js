d0d8e3ed81ed78770c4baa8ae451fbec7d8c0943
Josh Jeong <jcjeong@gmail.com>
2014-10-29 13:42:14 -0700

    Refactors- prototypes all functions

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,11 +1,13 @@
 var FINISHLINE = "830px"
 
-var pView = function(){
-  this.update= function(player, newPosition){
-    player.css('left', newPosition)
-  } 
+var pView = function(){};
 
-  this.gameover= function(player){
+pView.prototype = {
+  update: function(player, newPosition){
+    player.css('left', newPosition)
+  },
+
+  gameover: function(player){
     if(player.attr('id')=='player1'){
       $('#avatar').css("background-image", "url('images/squirtle_wins.gif')")
     }
@@ -13,46 +15,50 @@ var pView = function(){
       $('#avatar').css("background-image", "url('images/charmander_wins.gif')")
     }
     $('#gameover').show();
-  }
+  },
 
-  this.hideResult = function(){
+  hideResult:  function(){
     $('#gameover').hide();
-  }
+  },
 
-  this.resetPlayers = function(player1, player2){
+  resetPlayers:  function(player1, player2){
     player1.css('left', 0)
     player2.css('left', 0)
   }
+
 }
 
 
-var pController = function(pokemonView){
-  var self = this;
-      this.pokemonView = pokemonView
-      player1 = $('#player1')
-      player2 = $('#player2')
 
-  this.bindkeys = function(e){
+var pController = function(pokemonView){
+  this.pokemonView = pokemonView,
+  this.player1 = $('#player1'),
+  this.player2 = $('#player2');
+};
+
+
+pController.prototype = {
+  bindkeys: function(e){
     if(e.keyCode == 65){
-      position = this.currentPosition(player1)
-      this.move(player1, position)
+      var position = this.currentPosition(this.player1);
+      this.move(this.player1, position)
       this.zindex()
       this.moveGrass()
     }
     else if(e.keyCode == 76){
-      position = this.currentPosition(player2)
-      this.move(player2, position)
+      var position = this.currentPosition(this.player2);
+      this.move(this.player2, position)
       this.zindex()
       this.moveGrass()
     }
-  }
+  },
 
-  this.currentPosition= function(player){
+  currentPosition: function(player){
     return parseInt(player.css("left"))
-  }
+  },
 
-  this.move = function(player, position){
-    newPosition= position + 20 + "px"
+  move: function(player, position){
+    var newPosition= position + 20 + "px"
     if(parseInt(newPosition) >= parseInt(FINISHLINE)){
       this.pokemonView.gameover(player);
       setTimeout(this.reset, 3000);
@@ -60,34 +66,32 @@ var pController = function(pokemonView){
     else{
       this.pokemonView.update(player, newPosition)
     }
-  }
+  },
 
-  this.zindex = function(){
-    var player1Pos= parseInt(player1.css('left'))
-        player2Pos= parseInt(player2.css('left'))
+  zindex: function(){
+    var player1Pos= parseInt(this.player1.css('left')),
+        player2Pos= parseInt(this.player2.css('left'));
     if(player1Pos > player2Pos){
-      player1.css('z-index', player1Pos + 1)
+      this.player1.css('z-index', player1Pos + 1)
     }
     else{
-      player2.css('z-index', player2Pos + 1)
+      this.player2.css('z-index', player2Pos + 1)
     }
-  }
+  },
 
-  this.moveGrass = function(){
+  moveGrass: function(){
     var grassPos= parseInt($('#front-grass').css('left'))
     $('#front-grass').css('left', grassPos-2)
-  }
+  },
 
-  this.resetGrass = function(){
+  resetGrass: function(){
     $('#front-grass').css('left', 0)
-  }
+  },
 
-  this.reset = function(){
+  reset: function(){
+    var self = this;
     self.pokemonView.hideResult();
-    self.pokemonView.resetPlayers(player1, player2);
+    self.pokemonView.resetPlayers(this.player1, this.player2);
     self.resetGrass();
   }
-
-
-
-}
+};
